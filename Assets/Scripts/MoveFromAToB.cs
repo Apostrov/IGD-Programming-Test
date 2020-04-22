a090da5fc18be16a256aa2de6e62ad41a9c89ecc
Apostrov <amir.subaev@gmail.com>
2020-04-22 13:54:49 +0300

    Imort standart assets. Add moving enemy

---
+++ Assets/Scripts/MoveFromAToB.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFromAToB : MonoBehaviour
+{
+    public Transform left;
+    public Transform right;
+    public float speed;
+
+    private float nextX;
+
+    private void UpdateNextX()
+    {
+        if(Mathf.Abs(transform.position.x - left.position.x) < Mathf.Abs(transform.position.x - right.position.x))
+        {
+            nextX = right.position.x;
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else
+        {
+            nextX = left.position.x;
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+    
+    void Start()
+    {
+        UpdateNextX();
+    }
+
+    void Update()
+    {
+        if (Mathf.Abs(transform.position.x - nextX) < 1.0f)
+        {
+            UpdateNextX();
+        }
+        Vector3 newPos = new Vector3(nextX, transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * speed);
+    }
+}
---
+++ Assets/Standard Assets/2D/Scripts/Restarter.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityStandardAssets._2D
+{
+    public class Restarter : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+            }
+            else
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
